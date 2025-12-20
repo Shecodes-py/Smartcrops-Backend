6e617277cd00223dd85cf857a540aa2bbc32187b
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2025-12-20 11:56:27 +0100

    Import MultiPartParser and FormParser in views.py

--- endpoints/views.py
+++ endpoints/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from rest_framework.decorators import api_view #,  parser_classes
-# from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from PIL import Image # Pillow for image processing
