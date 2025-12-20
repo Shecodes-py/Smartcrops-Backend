fb861fba8e8006be79185828ca85b0b8d6e23341
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2025-12-20 12:01:41 +0100

    Add parser_classes decorator to api_view

--- endpoints/views.py
+++ endpoints/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-from rest_framework.decorators import api_view #,  parser_classes
+from rest_framework.decorators import api_view,  parser_classes
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
@@ -199,7 +199,6 @@ def analyze_with_ai_model(image):
 
 
 @csrf_exempt
-@require_http_methods(["POST"])
 @api_view(['POST'])
 def diagnose_plant(request):
     """
